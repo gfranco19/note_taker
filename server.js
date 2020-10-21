d0adfa1f6d1edda78eb810f908c93f4130e6130c
Giovanny Franco <gfranco19@live.com>
2020-10-21 14:38:26 -0700

    heroku

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const fs = require ("fs");
+const path = require ("path");
+
+const PORT = 3000;
+
+let app =express();
+let db = require ("./dev/db/db.json");
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const dbNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./dev/db/db.json"), (err, data) => {
+        if (err) throw err;
+    })
+);
+
+const dbUpdate = dbNotes => {
+    fs.writeFileSync(
+        path.join(__dirname, "./dev/db/db.json"),
+        JSON.stringify(dbNotes),
+        err => {
+            if (err) throw err;
+        }
+    );
+};
+
+// app.get requests for html pages //
+app.get("./dev/public/assets/css/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "./dev/public/assets/css/styles.css"));
+});
+
+app.get("./dev/public/index.js", (req,res) => {
+    res.sendFile(path.join(__dirname, "./dev/public/index.js"));
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./dev/public/index.html"));
+});
+
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./dev/public/notes.html"));
+});
+
+app.get("/api/notes", (req,res) => {
+    return res.json(dbNotes);
+});
+
+// request for posting and deleting the notes //
+
+app.post('/api/notes', (req,res) => {
+    let newNote = req.body;
+    let id = dbNotes.length;
+    newNote.id = id + 1;
+    dbNotes.push(newNote);
+    dbUpdate(dbNotes);
+    return res.json(dbNotes);
+});
+
+// this deletes the notes //
+app.delete("/api/notes/:id", (req,res) => {
+    let id = req.params.id;
+    let x = 1;
+    delete dbNotes[id - 1];
+    dbUpdate(dbNotes);
+    res.send(dbNotes);
+});
+
+
+// listener to PORT 3000 *todos: change to Proccess.env when deploying to heroku//
+
+app.listen(PORT, function () {
+    console.log("https://localhost:" + PORT + " is listening");
+})
